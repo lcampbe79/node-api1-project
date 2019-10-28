72a469ccccf2335cef03bbe6148cd85188960b91
Lisa Campbell <lisa@Lisas-MBP.lan>
2019-10-28 12:44:48 -0700

    Able to GET all users.

--- index.js
+++ index.js
@@ -3,11 +3,25 @@ const express = require('express');
 
 const server = express();
 
+const db = require('./data/db')
+
 server.use(express.json());
 
+//initial GET
 server.get('/', (req, res) => {
   res.send('Daily Challenge Node API 1!');
 });
+
+//GETs all users
+server.get('/api/users', (req, res) => {
+  db.find()
+  .then((users) => {
+    res.status(201).json(users)
+  })
+  .catch((err) => {
+    res.status(400).json({errorMessage: "Please provide name and bio for the user." })
+  })
+})
 
 server.listen(8000, () => {
   console.log('\n**Here comes the data!**\n')
