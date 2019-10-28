9d0cde1eb118daa9ddce8c47630b27d6bbcafbbe
Lisa Campbell <lisa@Lisas-MBP.lan>
2019-10-28 12:36:23 -0700

    Initial commit with basic GET.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // implement your API here
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send('Daily Challenge Node API 1!');
+});
+
+server.listen(8000, () => {
+  console.log('\n**Here comes the data!**\n')
+})
